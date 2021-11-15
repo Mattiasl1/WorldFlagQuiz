be1c2ba4aa485a5bbb18eebb0fb4d04a44f85fbc
Mattias Liljenberg <liljenbeerg@hotmail.com>
2021-11-15 12:46:52 +0100

    Initial Commit

---
+++ sadsadasd/sadsadasdApp.swift
@@ -0,0 +1,17 @@
+//
+//  sadsadasdApp.swift
+//  sadsadasd
+//
+//  Created by Mattias Liljenberg on 2021-11-15.
+//
+
+import SwiftUI
+
+@main
+struct sadsadasdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
